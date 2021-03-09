7f9515f5a6d3a442ce6cf31c0843104f6e174533
fgottes <f.gottesmann@outlook.com>
2021-03-09 09:36:11 +0100

    installing dependencies and create postcss.config.js file

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
+};
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  // ...
+};
